88b275d1743266260474aa6119afc5db7fe4e0df
John Romby <au518762@uni.au.dk>
2018-09-21 16:37:22 +0200

    A good attempt. Not testet yet though.

--- file_server/file_server.cs
+++ file_server/file_server.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace tcp
 {
@@ -27,8 +28,49 @@ namespace tcp
  		/// </summary>
 		private file_server ()
 		{
-			// TO DO Your own code
-		}
+		    var localAddress = IPAddress.Parse("127.0.0.1");
+
+		    try
+		    {
+		        var server = new TcpListener(localAddress, PORT);
+
+		        while (true)
+		        {
+                    //server.Start();
+		            //Console.WriteLine("Server started!");
+
+                    Console.WriteLine("Server - Waiting for new connection...");
+		            var client = server.AcceptTcpClient();
+		            Console.WriteLine("Server - Client connected!");
+
+                    // Modtager besked fra Client:
+		            var stream = client.GetStream();
+
+                    // Reads data from the client and prints it to console:
+		            var fileName = LIB.readTextTCP(stream);
+                    Console.WriteLine($"Stream received: {fileName}");
+
+                    // Checks to see if file exists and returns its size:
+		            var fileSize = LIB.check_File_Exists(fileName);
+
+		            LIB.writeTextTCP(stream, fileSize.ToString());
+
+		            if (fileSize != 0)
+		                SendFile(fileName, fileSize, stream);
+		            else
+		                Console.WriteLine("File doesn't exist!");
+
+                    client.Close();
+                    server.Stop();
+		        }
+		    }
+
+		    catch (SocketException e)
+		    {
+		        Console.WriteLine($"Socket exception: {e}");
+// Does server.Stop() run? Or does this potentially create memory leak???
+            }
+        }
 
 		/// <summary>
 		/// Sends the file.
@@ -42,9 +84,24 @@ namespace tcp
 		/// <param name='io'>
 		/// Network stream for writing to the client.
 		/// </param>
-		private void sendFile (String fileName, long fileSize, NetworkStream io)
+		private void SendFile (string fileName, long fileSize, NetworkStream io)
 		{
-			// TO DO Your own code
+		    var totalAmountSend = 0;
+
+		    var bytes = new byte[BUFSIZE];
+            var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+		    while (totalAmountSend < (int)fileSize)
+		    {
+		        var bytesRead = fs.Read(bytes, 0, BUFSIZE);
+
+		        io.Write(bytes, 0, bytesRead);
+		        totalAmountSend += bytesRead;
+
+		        Console.WriteLine(bytesRead);
+		    }
+
+            fs.Close();
 		}
 
 		/// <summary>
