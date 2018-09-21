bf452b269c6334966cea1b46581d0e6c1443a6eb
John Romby <au518762@uni.au.dk>
2018-09-21 14:32:04 +0200

    Trying out the tutorial linked to in the exercise

---
+++ Client/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        const int PORT = 9000;
+        const int BUFSIZE = 1000;
+
+        TcpClient clientSocket = new TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            var outStream = Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            var inStream = new byte[10025];
+            serverStream.Read(inStream, 0, clientSocket.ReceiveBufferSize);
+            var returndata = Encoding.ASCII.GetString(inStream);
+            Msg(returndata);
+            textBox2.Text = "";
+            textBox2.Focus();
+        }
+
+        public void Msg(string msg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + msg;
+        }
+    }
+}
--- file_server/file_server.cs
+++ file_server/file_server.cs
@@ -1,62 +1,115 @@
+// *** http://csharp.net-informations.com/communications/csharp-server-socket.htm ***
+
 using System;
-using System.IO;
 using System.Net;
+using System.Text;
 using System.Net.Sockets;
 
 namespace tcp
 {
 	class file_server
 	{
-		/// <summary>
-		/// The PORT
-		/// </summary>
 		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE
-		/// </summary>
 		const int BUFSIZE = 1000;
+	    const string IP = "127.0.0.1";
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_server"/> class.
-		/// Opretter en socket.
-		/// Venter på en connect fra en klient.
-		/// Modtager filnavn
-		/// Finder filstørrelsen
-		/// Kalder metoden sendFile
-		/// Lukker socketen og programmet
- 		/// </summary>
-		private file_server ()
-		{
-			// TO DO Your own code
-		}
+        /// <summary>
+        /// New file server.
+        /// </summary>
+		//private file_server ()
+		//{
+		//    var requestCount = 0;
+
+  //          TcpListener serverSocket = new TcpListener(8888);
+  //          TcpClient clientSocket = default(TcpClient);
+
+		//    serverSocket.Start();
+		//    Console.WriteLine(" >> Server Started");
+
+		//    clientSocket = serverSocket.AcceptTcpClient();
+		//    Console.WriteLine(" >> Accept connection from client");
+
+		//    while ((true))
+		//    {
+		//        try
+		//        {
+		//            requestCount = requestCount + 1;
+		//            NetworkStream networkStream = clientSocket.GetStream();
+		//            byte[] bytesFrom = new byte[10025];
+		//            networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+		//            string dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+		//            dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+		//            Console.WriteLine(" >> Data from client - " + dataFromClient);
+		//            string serverResponse = "Last Message from client" + dataFromClient;
+		//            Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+		//            networkStream.Write(sendBytes, 0, sendBytes.Length);
+		//            networkStream.Flush();
+		//            Console.WriteLine(" >> " + serverResponse);
+		//        }
+		//        catch (Exception ex)
+		//        {
+		//            Console.WriteLine(ex.ToString());
+		//        }
+		//    }
+
+		//    clientSocket.Close();
+		//    serverSocket.Stop();
+		//    Console.WriteLine(" >> exit");
+		//    Console.ReadLine();
+  //      }
 
 		/// <summary>
 		/// Sends the file.
 		/// </summary>
-		/// <param name='fileName'>
-		/// The filename.
-		/// </param>
-		/// <param name='fileSize'>
-		/// The filesize.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for writing to the client.
-		/// </param>
-		private void sendFile (String fileName, long fileSize, NetworkStream io)
-		{
-			// TO DO Your own code
-		}
+		//private void sendFile (String fileName, long fileSize, NetworkStream io)
+		//{
+		//	// TO DO Your own code
+		//}
 
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
+
+        // MAIN:
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Server starts...");
-			new file_server();
+            //new file_server();
+
+	        var ipAddress = IPAddress.Parse(IP);
+
+		    var serverSocket = new TcpListener(ipAddress, PORT);
+		    var requestCount = 0;
+		    serverSocket.Start();
+		    Console.WriteLine(" >> Server Started");
+		    var clientSocket = serverSocket.AcceptTcpClient();
+		    Console.WriteLine(" >> Accept connection from client");
+		    requestCount = 0;
+
+		    while ((true))
+		    {
+		        try
+		        {
+		            requestCount = requestCount + 1;
+		            NetworkStream networkStream = clientSocket.GetStream();
+		            byte[] bytesFrom = new byte[10025];
+		            networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+		            string dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+		            dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+		            Console.WriteLine(" >> Data from client - " + dataFromClient);
+		            string serverResponse = "Last Message from client" + dataFromClient;
+		            Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+		            networkStream.Write(sendBytes, 0, sendBytes.Length);
+		            networkStream.Flush();
+		            Console.WriteLine(" >> " + serverResponse);
+		        }
+		        catch (Exception ex)
+		        {
+		            Console.WriteLine(ex.ToString());
+		        }
+		    }
+
+		    clientSocket.Close();
+		    serverSocket.Stop();
+		    Console.WriteLine(" >> exit");
+		    Console.ReadLine();
 		}
 	}
 }
